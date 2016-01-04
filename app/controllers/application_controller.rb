c2aa9caa027168111ffc7d3a7de5791df2f86112
楠本哲也 <kusumoto@kusumototetsuya-no-Mac-mini.local>
2016-01-04 09:23:47 +0900

    add edit for staff account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  #include ErrorHandlers if Rails.env.production?
-  include ErrorHandlers
-  
+  include ErrorHandlers if Rails.env.production?
+  #include ErrorHandlers
+
   private
   def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
